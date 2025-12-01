d8f855c8b78bdddcfb2ceb54818e02dc1c57be09
Kieuviet34 <kieuviet34@gmail.com>
2025-12-01 08:09:28 +0700

    update services for gg oauth2

--- auth-service/src/main/java/learning/auth/services/AuthencationService.java
+++ auth-service/src/main/java/learning/auth/services/AuthencationService.java
@@ -1,5 +1,9 @@
 package learning.auth.services;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import jakarta.servlet.http.HttpServletRequest;
 import learning.auth.entity.AuthenticationRequest;
 import learning.auth.entity.AuthenticationResponse;
@@ -9,15 +13,20 @@ import learning.auth.entity.Role;
 import learning.auth.entity.User;
 import learning.auth.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
 @RequiredArgsConstructor
 public class AuthencationService {
+    @Value("${application.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
@@ -56,6 +65,42 @@ public class AuthencationService {
                 .refreshToken(refreshToken)
                 .build();
     }
+
+    public AuthenticationResponse authenticateGoogle(String googleToken){
+        try{
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                    .setAudience(Collections.singletonList(googleClientId)).build();
+            GoogleIdToken idToken = verifier.verify(googleToken);
+            if(idToken == null){
+                GoogleIdToken.Payload payload = idToken.getPayload();
+                String email = payload.getEmail();
+                String username = (String)payload.get("name");
+
+                var user = userRepository.findByEmail(email).orElse(null);
+
+                if(user == null){
+                    user = User.builder()
+                            .username(email)
+                            .email(email)
+                            .password(passwordEncoder.encode("GG_AUTH_NO_PASS"))
+                            .role(Role.USER)
+                            .build();
+                    userRepository.save(user);
+                }var jwtToken = jwtService.generateToken(user);
+                var refreshToken = jwtService.generateRefreshToken(user);
+
+                return AuthenticationResponse.builder()
+                        .accessToken(jwtToken)
+                        .refreshToken(refreshToken)
+                        .build();
+            }else {
+                throw new RuntimeException("Invalid Google Token");
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
     public AuthenticationResponse refreshToken(HttpServletRequest request){
         final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         final String refreshToken;
