f904e59b109b872d0cea61b9b291e99b08cadd77
Kieuviet34 <kieuviet34@gmail.com>
2025-12-07 05:59:54 +0700

    added redis config and redis caching for content service

--- content-service/src/main/java/learning/contentservice/ContentServiceApplication.java
+++ content-service/src/main/java/learning/contentservice/ContentServiceApplication.java
@@ -2,8 +2,10 @@ package learning.contentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ContentServiceApplication {
 
     public static void main(String[] args) {
