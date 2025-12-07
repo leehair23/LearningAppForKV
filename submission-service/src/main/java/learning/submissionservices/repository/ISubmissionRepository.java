fd875d85d86a48c75c1228d2d071190777868dde
Kieuviet34 <kieuviet34@gmail.com>
2025-12-08 05:38:34 +0700

    added rejudge for submission service

--- submission-service/src/main/java/learning/submissionservices/repository/ISubmissionRepository.java
+++ submission-service/src/main/java/learning/submissionservices/repository/ISubmissionRepository.java
@@ -3,6 +3,10 @@ package learning.submissionservices.repository;
 import learning.submissionservices.entity.Submission;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ISubmissionRepository extends MongoRepository<Submission, String> {
+import java.util.List;
 
+public interface ISubmissionRepository extends MongoRepository<Submission, String> {
+    List<Submission> findByProblemId(String problemId);
+
+    long countByStatus(String success);
 }
--- submission-service/src/main/java/learning/submissionservices/repository/SubmissionService.java
+++ submission-service/src/main/java/learning/submissionservices/repository/SubmissionService.java
@@ -5,8 +5,12 @@ import learning.submissionservices.dto.SubmissionbRequest;
 import learning.submissionservices.dto.runtime.CodeExecuteRequest;
 import learning.submissionservices.dto.runtime.CodeExecuteResponse;
 
+import java.util.Map;
+
 public interface SubmissionService {
     SubmissionResponse create(String id, SubmissionbRequest req);
     SubmissionResponse getById(String id);
     void processCallback(String id, CodeExecuteResponse response);
+    void rejudgeProblem(String id);
+    Map<String, Object> getSubmissionStats();
 }
