5f81c766a7d10e8763f49544859e07f5c096a453
Kieuviet34 <kieuviet34@gmail.com>
2025-12-07 14:55:18 +0700

    added redis config and pre implement leaderboard

--- user-service/src/main/java/learning/userservices/UserServicesApplication.java
+++ user-service/src/main/java/learning/userservices/UserServicesApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
         UserDetailsServiceAutoConfiguration.class})
 @EnableFeignClients
+@EnableCaching
 public class UserServicesApplication {
 
     public static void main(String[] args) {
