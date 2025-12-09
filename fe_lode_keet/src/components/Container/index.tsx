b76f2abbbcda4b2024917c067752be6052c65884
haleey23 <lehonghai2307.hdc@gmail.com>
2025-12-09 09:23:59 +0300

    Add small fix for Container component

--- fe_lode_keet/src/components/Container/index.tsx
+++ fe_lode_keet/src/components/Container/index.tsx
@@ -1,8 +1,15 @@
-const Container = (props: { children: React.ReactNode }) => (
-  <div className="flex flex-col items-center justify-center bg-slate-800">
-    {/* p-6  grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8  */}
-    {props.children}
-  </div>
-);
+const Container = (props: {
+  children: React.ReactNode;
+  additionalClassName?: string;
+}) => {
+  const { additionalClassName } = props;
+  const combinedClassName = `flex flex-col items-center justify-center bg-slate-800 ${additionalClassName}`;
+  return (
+    <div className={combinedClassName}>
+      {/* p-6  grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8  */}
+      {props.children}
+    </div>
+  );
+};
 
 export default Container;
